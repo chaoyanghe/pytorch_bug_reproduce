1a4a6cd56ffa529640164f4bac392c43697f5160
Chaoyang He <hchaoyan@amazon.com>
2021-09-14 16:29:08 -0700

    Update test_rpc_pipeline.py

--- test_rpc_pipeline.py
+++ test_rpc_pipeline.py
@@ -1,12 +1,15 @@
 import argparse
 import logging
 import os
+import time
+from datetime import timedelta
 
 import torch
 import torch.distributed as dist
 from torch import nn
 from torch.distributed import Backend, rpc
 from torch.distributed.pipeline.sync import Pipe
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 
 def add_args():
@@ -55,7 +58,6 @@ class DistManager:
         self._init_rpc()
 
     def _init_ddp(self):
-        self.local_rank = self.local_rank
         logging.info(f"Running DP on local rank {self.local_rank}.")
 
         self.master_port += 1
@@ -94,18 +96,35 @@ class DistManager:
         # [BC-Breaking][RFC] Retire ProcessGroup Backend for RPC #55615
         str_init_method = "tcp://" + str(self.master_addr) + ":10000"
         logging.info("str_init_method = {}".format(str_init_method))
-        options = rpc.TensorPipeRpcBackendOptions(
-            num_worker_threads=16, rpc_timeout=20, init_method=str_init_method
+        options = rpc.ProcessGroupRpcBackendOptions(
+            num_send_recv_threads=4, rpc_timeout=0.0, init_method=str_init_method
         )
         rpc.init_rpc(
             "worker:" + str(self.global_rank),
-            backend=rpc.BackendType.TENSORPIPE,
+            backend=dist.rpc.BackendType.PROCESS_GROUP,
             rank=self.global_rank,
             world_size=self.world_size,
             rpc_backend_options=options,
         )
         # torch.distributed.rpc.init_rpc('worker', rank=self.global_rank, world_size=self.world_size)
         logging.info("init_rpc finished.")
+
+    def generate_ddp_model(self, model):
+        # all_reduce group
+        ddp_ranks = [rank for rank in range(self.world_size)]
+        ddp_group = dist.new_group(
+            ranks=ddp_ranks,
+            backend=Backend.NCCL,
+            timeout=timedelta(days=365),
+        )
+        model = DDP(model, process_group=ddp_group)
+        return model
+
+    def get_global_rank(self):
+        return self.global_rank
+
+    def get_lobal_rank(self):
+        return self.local_rank
 
 
 class PipeModelWrapper(nn.Module):
@@ -133,17 +152,69 @@ if __name__ == "__main__":
         master_port=args.master_port,
     )
     dist_mgr.init()
+    global_rank = dist_mgr.get_global_rank()
+    local_rank = dist_mgr.get_lobal_rank()
 
-    hidden_size = 1024
-    # Step 1: build a model including two linear layers
-    fc1 = nn.Linear(hidden_size, hidden_size).cuda(0)
-    fc2 = nn.Linear(hidden_size, hidden_size).cuda(1)
-    # Step 2: wrap the two layers with nn.Sequential
-    pipeline_model = nn.Sequential(fc1, fc2)
+    batch_size = 8
+    max_seq_length = 2
+    hidden_size = 4
 
-    # Step 3: build Pipe (torch.distributed.pipeline.sync.Pipe)
-    pipeline_model = PipeModelWrapper(Pipe(pipeline_model, chunks=4, checkpoint="never"))
+    device = torch.device("cuda:" + str(local_rank))
 
-    device = torch.device("cuda:0")
-    pipeline_model.to(device)
-    logging.info(pipeline_model)
+    def prepare_training_data(batch_size, max_seq_length, hidden_size):
+        # logging.info(
+        #     "batch_size = {}, max_seq_length = {}, hidden_size = {}".format(
+        #         batch_size,
+        #         max_seq_length,
+        #         hidden_size,
+        #     )
+        # )
+        x_dummpy = torch.rand(batch_size, max_seq_length, hidden_size)
+        return x_dummpy
+
+
+    class SuperModel(nn.Module):
+        def __init__(self, hidden_size):
+            super().__init__()
+
+            # Step 1: build a model including two linear layers
+            fc1 = nn.Linear(hidden_size, hidden_size).cuda(local_rank)
+            fc2 = nn.Linear(hidden_size, hidden_size).cuda(local_rank)
+            # Step 2: wrap the two layers with nn.Sequential
+            pipeline_model = nn.Sequential(fc1, fc2)
+
+            # Step 3: build Pipe (torch.distributed.pipeline.sync.Pipe)
+            self.pipeline_model = PipeModelWrapper(Pipe(pipeline_model, chunks=4, checkpoint="never"))
+
+            self.dense = nn.Linear(hidden_size, hidden_size)
+
+        def forward(self, hidden_states):
+            pipeline_output = self.pipeline_model(hidden_states)
+            output = self.dense(pipeline_output)
+            return output
+
+    super_model = SuperModel(hidden_size)
+    super_model.to(device)
+
+    super_model = dist_mgr.generate_ddp_model(super_model)
+    if global_rank == 0:
+        logging.info(super_model)
+
+    ITER_NUM = 10
+    super_model.train()
+    time_cost_per_iter_total = 0.0
+    for iter_idx in range(ITER_NUM):
+        x = prepare_training_data(batch_size, max_seq_length, hidden_size)
+        x = x.to(device)
+        output = super_model(x)
+
+        # define a loss
+        loss = torch.sum(output)
+        loss.backward()
+
+        if global_rank == 0:
+            logging.info("loss = {}".format(loss))
+
+
+
+
